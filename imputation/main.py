108b3ba44a1fea1beb3dabcb72269a35ef3ababc
Huy Nguyen <huy.nguyen@ihsmarkit.com>
2022-12-14 16:28:54 -0600

    toy imputation code

---
+++ imputation/main.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import load_diabetes
+
+
+rng = np.random.RandomState(42)
+
+X_diabetes, y_diabetes = load_diabetes(return_X_y=True)
+X_california, y_california = fetch_california_housing(return_X_y=True)
+X_california = X_california[:300]
+y_california = y_california[:300]
+X_diabetes = X_diabetes[:300]
+y_diabetes = y_diabetes[:300]
+
+def add_missing_values(X_full, y_full):
+    n_samples, n_features = X_full.shape
+
+    # Add missing values in 75% of the lines
+    missing_rate = 0.75
+    n_missing_samples = int(n_samples * missing_rate)
+
+    missing_samples = np.zeros(n_samples, dtype=bool)
+    missing_samples[:n_missing_samples] = True
+
+    rng.shuffle(missing_samples)
+    missing_features = rng.randint(0, n_features, n_missing_samples)
+    X_missing = X_full.copy()
+    X_missing[missing_samples, missing_features] = np.nan
+    y_missing = y_full.copy()
+
+    return X_missing, y_missing
+
+
+X_miss_california, y_miss_california = add_missing_values(X_california, y_california)
+
+X_miss_diabetes, y_miss_diabetes = add_missing_values(X_diabetes, y_diabetes)
+
+# pd.DataFrame(X_miss_california).isnull().sum()
+# pd.Series(y_miss_california).isnull().sum()
+# pd.DataFrame(X_miss_diabetes).isnull().sum()
+# pd.Series(y_miss_diabetes).isnull().sum()
+
+
+# Impute the missing data and score
+
+rng = np.random.RandomState(0)
+from sklearn.ensemble import RandomForestRegressor
+# To use the experimental IterativeImputer, we need to explicitly ask for it:
+from sklearn.experimental import enable_iterative_imputer  # noqa
+from sklearn.impute import SimpleImputer, KNNImputer, IterativeImputer
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import make_pipeline
+
+
+N_SPLITS = 4
+regressor = RandomForestRegressor(random_state=0)
+
+def get_scores_for_imputer(imputer, X_missing, y_missing):
+    estimator = make_pipeline(imputer, regressor)
+    impute_scores = cross_val_score(
+        estimator, X_missing, y_missing, scoring="neg_mean_squared_error", cv=N_SPLITS
+    )
+    return impute_scores
+
+x_labels = []
+mses_california = np.zeros(5)
+stds_california = np.zeros(5)
+mses_diabetes = np.zeros(5)
+stds_diabetes = np.zeros(5)
+
+def get_full_score(X_full, y_full):
+    full_scores = cross_val_score(
+        regressor, X_full, y_full, scoring="neg_mean_squared_error", cv=N_SPLITS
+    )
+    return full_scores.mean(), full_scores.std()
+
+
+mses_california[0], stds_california[0] = get_full_score(X_california, y_california)
+mses_diabetes[0], stds_diabetes[0] = get_full_score(X_diabetes, y_diabetes)
+x_labels.append("Full data")
+
+# Iterative imputation of the missing values
+
+def get_impute_iterative(X_missing, y_missing):
+    imputer = IterativeImputer(
+        missing_values=np.nan,
+        add_indicator=True,
+        random_state=0,
+        n_nearest_features=3,
+        max_iter=1,
+        sample_posterior=True,
+    )
+    iterative_impute_scores = get_scores_for_imputer(imputer, X_missing, y_missing)
+    return iterative_impute_scores.mean(), iterative_impute_scores.std()
+
+
+mses_california[4], stds_california[4] = get_impute_iterative(
+    X_miss_california, y_miss_california
+)
+mses_diabetes[4], stds_diabetes[4] = get_impute_iterative(
+    X_miss_diabetes, y_miss_diabetes
+)
+x_labels.append("Iterative Imputation")
+
+mses_diabetes = mses_diabetes * -1
+mses_california = mses_california * -1
